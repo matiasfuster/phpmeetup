74ea72935e1e66aa6cc687ccc8bfe5052018687b
Matias Fuster <matias.fuster@solcre.com>
2015-08-08 00:01:11 -0300

    Config

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 include __DIR__ . "/../vendor/autoload.php";
-include __DIR__ . "config.php";
+include __DIR__ . "/config.php";
 
 use PHPMeetup\Draw\ProbabilityCalculator;
 use PHPMeetup\Exception\ExceptionInterface as PHPMeetupException;
